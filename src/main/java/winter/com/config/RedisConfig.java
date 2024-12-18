6ccb647a79f989ef02e22bf7b111db101641422b
강재현 <recreate.kang@gmail.com>
2024-12-19 06:17:45 +0900

    refactor: config - 환경정보 은닉

--- src/main/java/winter/com/config/RedisConfig.java
+++ src/main/java/winter/com/config/RedisConfig.java
@@ -1,8 +1,8 @@
 package winter.com.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,19 +11,21 @@ import winter.com.vo.KeyWord;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
 
     @Bean
     LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
 
     @Bean
-    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+    RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(lettuceConnectionFactory());
-
         template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(KeyWord.class));
-
         return template;
     }
 }
